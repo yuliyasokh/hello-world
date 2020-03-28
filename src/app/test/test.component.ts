018cfaa7dff44184b949bbe0879f8a4d5da103df
yuliya.sokh@gmail.com <21014151y>
2020-03-28 14:50:05 +0100

    initial commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-test]',
+  template: `
+    <h2>
+      Welcome {{name}}
+    </h2>
+    <h2>{{name.toUpperCase()}}</h2>
+    <h2>{{siteUrl}}</h2>
+
+    <h2>{{greetUser()}}</h2>
+    <input [id]="myId" type="text" value="Vishla">
+  `,
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+  public name= "Julia";
+  public myId= "testId";
+  public siteUrl = window.location.href;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public greetUser(){
+    return "Iuliia"
+}
+
+}
